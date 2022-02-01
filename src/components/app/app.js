2e6b1b22a1701fd92d65f6fc5c0b9f0e5659425d
Sergey Kuznetsov <mr.enline@gmail.com>
2022-02-01 19:57:38 +0300

    добавлены ключи в массив объектов data для того, чтобы react перерисовывал только те элементы, которые рально изменелись. См. алгоритм согласования

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,10 +9,15 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 
 function App() {
+    //для того, чтобы реакт перерисовывал только те компоненты, которые реально изменелись,
+    //добавляют параметр key или id(они будут приходить с сервера)
+    //реакт будет сравнивать текущий элемент с тем, что был раньше и если он изменился и расположен
+    //не в конце, а в середине или в начале, то он перерисует только то что изменилось благодаря id
+    //данный режим в реакте называется РЕЖИМ СОГЛАСОВАНИЯ
     const data = [
-        {name: 'Vasya', salary: 800, increase: false},
-        {name: 'Petya', salary: 1800, increase: true},
-        {name: 'Kolya', salary: 2500, increase: false}
+        {name: 'Vasya', salary: 800, increase: false, id: 1},
+        {name: 'Petya', salary: 1800, increase: true, id: 2},
+        {name: 'Kolya', salary: 2500, increase: false, id: 3}
     ];
 
     return (
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -6,12 +6,25 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 const EmployeesList = ({data}) => {
 
     const elements = data.map(item => {
+        const {id, ...itemProps} = item; //остаточный принцип. Сначала в item ищется id и записывается в переменную id
+                                        // затем все остальные значения переменных записываются в itemProps
+
         return (
             //<EmployeesListItem name = {item.name} salary={item.salary}/>
-            <EmployeesListItem {...item}/>  //спред-оператор идентичен строке выше
+            //<EmployeesListItem {...item}/>  //спред-оператор идентичен строке выше
+            <EmployeesListItem key={id}  {...itemProps}/>
         )
     })
 
+    //2-й способо задания id, если с сервера такие данные не приходят вообще
+    //i - индекс элемента и он будет id
+    // const elements = data.map((item, i) => {
+    //     const {id, ...itemProps} = item; 
+    //     return (
+    //         <EmployeesListItem key={i}  {...itemProps}/>
+    //     )
+    // })
+          
     return (
         <ul className="app-list list-group">
             {elements}
